888e29a59ce9b916e2ac2cad03e6c0685235fe5e
Michael Irigoyen <mkevinirigoyen@gmail.com>
2024-03-16 02:39:53 -0300

    commit

--- src/main/java/com/backend/Odontologo/service/impl/OdontologoService.java
+++ src/main/java/com/backend/Odontologo/service/impl/OdontologoService.java
@@ -2,9 +2,16 @@
 package com.backend.Odontologo.service.impl;
 
 
+import com.backend.Odontologo.dto.entrada.OdontologoEntradaDto;
+import com.backend.Odontologo.dto.entrada.PacienteEntradaDto;
+import com.backend.Odontologo.dto.salida.OdontologoSalidaDto;
+import com.backend.Odontologo.dto.salida.PacienteSalidaDto;
 import com.backend.Odontologo.entity.Odontologo;
+import com.backend.Odontologo.entity.Paciente;
 import com.backend.Odontologo.repository.OdontologoRepository;
+import com.backend.Odontologo.repository.PacienteRepository;
 import com.backend.Odontologo.service.IOdontologoService;
+import com.backend.Odontologo.utils.JsonPrinter;
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,28 +21,57 @@ import java.util.List;
 public abstract class OdontologoService implements IOdontologoService {
     private final Logger LOGGER = LoggerFactory.getLogger(OdontologoService.class);
     private OdontologoRepository odontologoRepository;
-
-
     private ModelMapper modelMapper;
 
     public OdontologoService(OdontologoRepository odontologoRepository, ModelMapper modelMapper) {
         this.odontologoRepository = odontologoRepository;
         this.modelMapper = modelMapper;
+        configureMapping();
     }
 
-    public Odontologo registrarOdontologo(Odontologo odontologo) {
-        return odontologoRepository.save(odontologo);
+    @Override
+    public OdontologoSalidaDto registrarOdontologo(OdontologoEntradaDto odontologo) {
+        LOGGER.info("OdontologoEntradaDto: {}", JsonPrinter.toString(odontologo));
+        Odontologo OdontologoEntidad = modelMapper.map(odontologo, Odontologo.class);
+        Odontologo odontologoEntidadConId = odontologoRepository.save(OdontologoEntidad);
+        OdontologoSalidaDto odontologoSalidaDto = modelMapper.map(odontologoEntidadConId, OdontologoSalidaDto.class);
+        LOGGER.info("OdontologoSalidaDto: {}",  JsonPrinter.toString(odontologoSalidaDto));
+        return odontologoSalidaDto;
     }
 
-    public Odontologo buscarOdontologoPorId(Long id) {
-        return odontologoRepository.findById(id).orElse(null);
+    @Override
+    public List<PacienteSalidaDto> listarPacientes() {
+        List<PacienteSalidaDto> pacientesSalidaDto =  pacienteRepository.findAll()
+                .stream()
+                .map(paciente -> modelMapper.map(paciente, PacienteSalidaDto.class))
+                .toList();
+
+        LOGGER.info("Listado de todos los pacientes: {}", JsonPrinter.toString(pacientesSalidaDto));
+        return pacientesSalidaDto;
     }
 
-    public List<Odontologo> listarOdontologo() {
-        return odontologoRepository.findAll();
+    @Override
+    public PacienteSalidaDto buscarPacientePorId(Long id) {
+        Paciente pacienteBuscado = pacienteRepository.findById(id).orElse(null);
+        PacienteSalidaDto pacienteEncontrado = null;
+
+        if(pacienteBuscado != null){
+            pacienteEncontrado = modelMapper.map(pacienteBuscado, PacienteSalidaDto.class);
+            LOGGER.info("Paciente encontrado: {}", JsonPrinter.toString(pacienteEncontrado));
+        } else {
+            LOGGER.error("El id no se encuentra registrado en la base de datos");
+        }
+
+        return pacienteEncontrado;
     }
 
+    private void configureMapping(){
+        modelMapper.typeMap(PacienteEntradaDto.class, Paciente.class)
+                .addMappings(mapper -> mapper.map(PacienteEntradaDto::getDomicilioEntradaDto, Paciente::setDomicilio));
 
+        modelMapper.typeMap(Paciente.class, PacienteSalidaDto.class)
+                .addMappings(mapper -> mapper.map(Paciente::getDomicilio, PacienteSalidaDto::setDomicilioSalidaDto));
+    }
 
 
 }
